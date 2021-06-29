8b390a3eadafd8c32e5ccb36dec44dbee68a3e61
Aegide <geoffroygressier1@yahoo.fr>
2021-06-30 00:27:07 +0200

    Add google sheets integration

---
+++ test.py
@@ -0,0 +1,48 @@
+import gspread
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+
+spreadsheet_name = "Pokemon IF Sprite Completion (discord bot)"
+worksheet_name = "TestSheet"
+row_fusion_init = 6
+col_fusion_init = 3
+
+no_fusion = ""
+valid_fusion = "x"
+approved_fusion = "✓"
+
+scope = None
+creds = None
+client = None
+sheet = None
+wks = None
+
+def init():
+    successful_init = True
+    try:
+        global scope, creds, client, sheet, wks
+        scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+        creds = ServiceAccountCredentials.from_json_keyfile_name('token.json', scope)
+        client = gspread.authorize(creds)
+        sheet = client.open(spreadsheet_name)
+        wks = sheet.worksheet(worksheet_name)
+
+    except Exception as e:
+        print("Exception :", e, "\n")
+        successful_init = False
+    
+    return successful_init
+
+def get_fusion_data(head_id, body_id):
+    row = row_fusion_init + body_id
+    col = col_fusion_init + head_id
+    return gspread.models.Worksheet.cell(wks, row, col)
+
+def set_fusion_data(head_id, body_id, new_value):
+    row = row_fusion_init + body_id
+    col = col_fusion_init + head_id
+    cell = gspread.utils.rowcol_to_a1(row, col)
+    wks.update_acell(cell, new_value)
+
+def validate_fusion(head_id, body_id):
+    return set_fusion_data(head_id, body_id, valid_fusion)
