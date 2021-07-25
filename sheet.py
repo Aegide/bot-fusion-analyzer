75165b11a7c8c611801d4ff91ed340d82a576d9a
Aegide <geoffroygressier1@yahoo.fr>
2021-07-25 17:51:55 +0200

    Add gspread-related features

--- sheet.py
+++ sheet.py
@@ -3,7 +3,6 @@ import pandas as pd
 from oauth2client.service_account import ServiceAccountCredentials
 
 spreadsheet_name = "Pokemon IF Sprite Completion (discord bot)"
-# worksheet_name = "Full dex"
 row_fusion_init = 9
 col_fusion_init = 8
 
@@ -31,14 +30,31 @@ def init(worksheet_name):
         successful_init = False
     return successful_init
 
-
 def get_fusion_data_by_fusion_id(fusion_id):
     split_fusion_id = fusion_id.split(".")
     if len(split_fusion_id) == 2:
         head_id, body_id = int(split_fusion_id[0]), int(split_fusion_id[1])
         return get_fusion_data(head_id, body_id)
-    
-    
+    else:
+        print("ERROR", "get_fusion_data_by_fusion_id", fusion_id)
+
+def get_valid_cell_by_fusion_id(fusion_id):
+    split_fusion_id = fusion_id.split(".")
+    if len(split_fusion_id) == 2:
+        head_id, body_id = int(split_fusion_id[0]), int(split_fusion_id[1])
+        return get_valid_cell(head_id, body_id)
+    else:
+        print("ERROR", "get_cell_by_fusion_id", fusion_id)
+
+def get_valid_cell(head_id, body_id):
+    row, col = row_fusion_init + body_id, col_fusion_init + head_id
+    try:
+        result = gspread.models.Cell(row, col, approved_fusion)
+    except Exception as e :
+        print(e)
+        result = "API ERROR"
+    return result
+
 def get_fusion_data(head_id, body_id):
     row, col = row_fusion_init + body_id, col_fusion_init + head_id
     try:
@@ -49,6 +65,8 @@ def get_fusion_data(head_id, body_id):
     return result
 
 def set_fusion_data(head_id, body_id, new_value):
+    head_id = int(head_id)
+    body_id = int(body_id)
     row = row_fusion_init + body_id
     col = col_fusion_init + head_id
     if wks.col_count >= col and wks.row_count >= row: 
@@ -56,6 +74,9 @@ def set_fusion_data(head_id, body_id, new_value):
         wks.update_acell(cell, new_value)
     else:
         print("set_fusion_data", head_id, body_id)
+
+def set_fusion_data_from_list(list_cells):
+    wks.update_cells(list_cells)
 
 def validate_fusion(fusion_id):
     split_fusion_id = fusion_id.split(".")
