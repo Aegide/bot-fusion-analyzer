9162d6c12b8b07bfb4f8ba8c73f397379a735c77
Aegide <geoffroygressier1@yahoo.fr>
2021-09-26 13:06:10 +0200

    Upate test

--- test.py
+++ test.py
@@ -1,18 +1,4 @@
 import os
-from os import listdir
-from os.path import isfile, join
 
 token = os.environ['GSHEET_KEY']
 print(token)
-
-text_file = open("token.json", "wt")
-text_file.write(token)
-text_file.close()
-
-mypath = "."
-# onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-
-print("-")
-for element in listdir(mypath):
-    print(element)
-print("-")
