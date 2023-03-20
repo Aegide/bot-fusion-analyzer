af91be81af913921a91b86829083e03f33a0ef1f
Aegide <geoffroygressier1@yahoo.fr>
2023-03-20 01:48:48 +0100

    Update test

--- bot/test.py
+++ bot/test.py
@@ -3,7 +3,7 @@ from typing import Any
 import unittest
 
 from PIL.ImagePalette import ImagePalette
-from PIL.Image import Image, open
+from PIL.Image import Image, open, new
 from PIL.PyAccess import PyAccess
 from utils import get_fusion_id_from_filename as gfiff
 
@@ -13,11 +13,13 @@ UPPER_COLOR_LIMIT = 1000
 PINK = (255, 0, 255, 255)
 BLACK = (0, 0, 0, 255)
 WHITE = (255, 255, 255, 255)
-
+RED = (255, 0, 0, 255)
+GREEN = (0, 255, 0, 255)
 
 MAX_TRANSPARENCY = 0
 MIN_TRANSPARENCY = 255
-
+STEP = 3
+MAX_SIZE = 288
 
 RGB_MODE = "RGB"
 RGBA_MODE = "RGBA"
@@ -55,16 +57,16 @@ PALETTE_MODE = "P"
 #                 useful_colors = remove_useless_colors(colors)
 
 
-class TestTransparencyAmount(unittest.TestCase):
-    def test_colour_amount(self):
-        sprites = os.listdir("fixtures")
-        for sprite in sprites:
-            sprite_path = os.path.join("fixtures", sprite)
-            with open(sprite_path) as image:
-                palette = get_palette(image)
-                pixels = get_pixels(image)
-                update_transparency(pixels, image.mode, palette)
-                # image.show()
+# class TestTransparencyAmount(unittest.TestCase):
+#     def test_colour_amount(self):
+#         sprites = os.listdir("fixtures")
+#         for sprite in sprites:
+#             sprite_path = os.path.join("fixtures", sprite)
+#             with open(sprite_path) as image:
+#                 palette = get_palette(image)
+#                 pixels = get_pixels(image)
+#                 update_transparency(pixels, image.mode, palette)
+#                 # image.show()
 
 
 def get_pixels(image:Image) -> PyAccess:
@@ -116,6 +118,81 @@ def update_palette(pixels:PyAccess, palette:ImagePalette):
             if pixel_color not in color_set:
                 color_set.add(pixel_color)
                 # print(palette_index, (i, j), pixel_color)
+
+
+class TestHalfPixels(unittest.TestCase):
+    def test_half_pixels(self):
+        sprites = os.listdir("fixtures")
+        for sprite in sprites:
+            sprite_path = os.path.join("fixtures", sprite)
+            with open(sprite_path) as image:
+                print(sprite, image.mode)
+                pixels = get_pixels(image)
+                update_half_pixels(pixels)
+                # image.show()
+
+
+def find_first_pixel(pixels:PyAccess):
+    default_value = pixels[0, 0]
+    count = 0
+    for i in range(0, 288):
+        for j in range(0, 288):
+            if default_value != pixels[i, j]:
+                return (i%3, j%3)
+    return (0, 0)
+
+
+def update_half_pixels(pixels:PyAccess):
+
+    exposed_image = new(RGBA_MODE, (MAX_SIZE, MAX_SIZE))
+    exposed_pixels = get_pixels(exposed_image)
+
+    (delta_i, delta_j) = find_first_pixel(pixels)
+
+    print((delta_i, delta_j), pixels[delta_i, delta_j])
+    print(" ")
+
+    issue_counter = 0
+
+    max_i = MAX_SIZE - (STEP - delta_i)
+    max_j = MAX_SIZE - (STEP - delta_j)
+    for i in range(delta_i, max_i, STEP):
+        for j in range(delta_j, max_j, STEP):
+    
+            color_set = get_color_set(i, j, pixels)
+            color = get_color_from_set(color_set)
+            recolor_pixels(i, j, exposed_pixels, color)
+                    
+            if color == RED:
+                print((i, j), color_set)
+                issue_counter += 1
+
+    if issue_counter > 0:
+        exposed_image.show()
+
+
+def get_color_from_set(color_set:set):
+    if len(color_set) > 1:
+        return RED
+    return GREEN
+
+
+def get_color_set(i:int, j:int, pixels:PyAccess):
+    color_set = set()
+    for increment_i in range(0, STEP):
+        for increment_j in range(0, STEP):
+            local_i = i + increment_i
+            local_j = j + increment_j
+            color_set.add(pixels[local_i, local_j])
+    return color_set
+
+
+def recolor_pixels(i:int, j:int, pixels:PyAccess, color:tuple):
+    for increment_i in range(0, STEP):
+        for increment_j in range(0, STEP):
+            local_i = i + increment_i
+            local_j = j + increment_j
+            pixels[local_i, local_j] = color
 
 
 if __name__ == '__main__':
