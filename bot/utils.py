166eaf13a4cb070b3df640a2fee7b8788324ae79
Aegide <geoffroygressier1@yahoo.fr>
2023-10-28 13:40:01 +0200

    Update AUTOGEN_FUSION_URL

--- bot/utils.py
+++ bot/utils.py
@@ -24,7 +24,9 @@ REGULAR_PATTERN_FUSION_ID = rf'^{STRICT_PATTERN_FUSION_ID}'
 SPOILER_PATTERN_FUSION_ID = rf'^SPOILER_{STRICT_PATTERN_FUSION_ID}'
 
 RAW_GITHUB = "https://raw.githubusercontent.com"
-AUTOGEN_FUSION_URL = f"{RAW_GITHUB}/infinitefusion/autogen-fusion-sprites/master/Battlers/"
+RAW_GITLAB = "https://gitlab.com"
+
+AUTOGEN_FUSION_URL = f"{RAW_GITLAB}/pokemoninfinitefusion/autogen-fusion-sprites/-/raw/master/Battlers/"
 QUESTION_URL = f"{RAW_GITHUB}/Aegide/bot-fusion-analyzer/main/bot/question.png"
 
 YAGPDB_ID = 204255221017214977
